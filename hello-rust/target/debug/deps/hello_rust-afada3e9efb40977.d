11c579a2a9db9bb399ac32c860d2ba90eb248c3a
Abhik Ray <abhikaboy@gmail.com>
2025-12-19 23:51:51 -0500

    removed targets lol

--- hello-rust/target/debug/deps/hello_rust-afada3e9efb40977.d
+++
@@ -1,5 +0,0 @@
-/Users/abhikray/Documents/Advent Of Code /hello-rust/target/debug/deps/hello_rust-afada3e9efb40977.d: src/main.rs
-
-/Users/abhikray/Documents/Advent Of Code /hello-rust/target/debug/deps/libhello_rust-afada3e9efb40977.rmeta: src/main.rs
-
-src/main.rs:
--- hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-/Users/abhikray/Documents/Advent\ Of\ Code\ /hello-rust/target/debug/hello-rust: /Users/abhikray/Documents/Advent\ Of\ Code\ /hello-rust/src/main.rs
